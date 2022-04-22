2e67eba8dab443933b30cf785ae94791190b8ecf
EVG2055 <enivanoff55@mail.ru>
2022-04-22 15:00:47 +0600

    done 91 of 312

--- archive/script 91 of 312.js
+++ archive/script 91 of 312.js
@@ -1,74 +1,28 @@
-// Выведите на экран следующую пирамидку:
+// 91 of 312
+// С помощью цикла сформируйте следующий массив:
+// ['xx', 'xxxx', 'xxxxxx', 'xxxxxxxx', 'xxxxxxxxxx']
 
-// xx
-// xxxx
-// xxxxxx
-// xxxxxxxx
-// xxxxxxxxxx
-
+// let arr = [];
 // let str = '';
 
-// for (let i = 1; i <= 5; i++) {
-// 	str += 'xx';
-// 	document.write(str + '<br>');
+// for (let i = 0; i < 5; i++) {
+//     str += 'xx';
+//     arr.push(str);
 // }
 
-// for (let i = 1; i <= 9; i++) {
-// 	for (let j= 1; j <= 3; j++) {
-// 		document.write(i);
-// 	}
-// 	document.write('<br>');
+// console.log(arr);
+
+// С помощью цикла сформируйте следующий массив:
+
+// ['11111', '22222', '33333', '44444', '55555']
+
+// let arr = [];
+
+// for (let i = 1; i <=5; i++) {
+//     let str = '';
+//     for (let j = 0; j < 3; j++) {
+//         str += i;
+//     }
+//     arr.push(str);
 // }
-
-// Выведите на экран следующую пирамидку:
-
-// 1
-// 333
-// 55555
-// 7777777
-// 999999999
-
-// for (let i = 1; i <= 9; i++) {
-// 	if(i % 2 !== 0) {
-// 		for (let j = 1; j <= i; j++){
-// 			document.write(i);
-// 		}
-// 		document.write('<br>');
-// 	}
-// }
-
-// Выведите на экран следующую пирамидку:
-
-// xxxxxxxxxx
-// xxxxxxxx
-// xxxxxx
-// xxxx
-// xx
-
-// for (let i = 6; i >= 1; i--) {
-// 	let str = '';
-// 	for (let j = 1; j < i; j++) {
-// 		str += 'xx';
-// 	}
-// 	document.write(str + '<br>');
-// }
-
-// Выведите на экран следующую пирамидку:
-
-// 999999999
-// 88888888
-// 7777777
-// 666666
-// 55555
-// 4444
-// 333
-// 22
-// 1
-
-// for (let i = 9; i >= 0; i--) {
-// 	let str = '';
-// 	for (let j = 0; j < i; j++) {
-// 		str += i;
-// 	}
-// 	document.write(str + '<br>');
-// }
+// console.log(arr);
