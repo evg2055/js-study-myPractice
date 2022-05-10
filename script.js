a85a03776308191d0aac90ca2c39fe60ecd97d3d
EVG2055 <enivanoff55@mail.ru>
2022-05-10 22:29:54 +0600

    done get LuckyTicket

--- script.js
+++ script.js
@@ -1,39 +1,88 @@
-function getLackyTickets() {
-	let result = [];
+function getLackyTicket(digitsAmount) {
+    let result = [];
+    let first = startRange (digitsAmount);
+    let finish = endRange (digitsAmount);
 
-	for (let i = 1001; i < 1000000; i++) {
-		if (lackyIs(i)) {
-			result.push(normalizeNum(i));
-		}
-	}
+    console.log(first);
+    console.log(finish);
 
-	return result;
+    for (let i = first ; i < finish; i++) {
+        if (lackyIs(normalizeNum(i, digitsAmount))) {
+            result.push(normalizeNum(i, digitsAmount));
+        }
+    }
+
+    return result;
 }
 
-console.log(getLackyTickets());
+console.log(getLackyTicket(10));
 
-function lackyIs(num) {
-	let str = String(normalizeNum(num));
+function lackyIs(num, digitsAmount) {
+    let sum1 = 0;
+    let sum2 = 0;
+    // console.log(num);
+    num = String(normalizeNum(num, digitsAmount));
+    // console.log(num);
 
-	let sum1 = +(str[0]) + +(str[1]) + +(str[2]);
-	let sum2 = +(str[3]) + +(str[4]) + +(str[5]);
+    for (let i = 0; i < num.length ; i++) {
+        // console.log(i, sum1, sum2);
 
-	return sum1 == sum2;
+        if (num.length / 2 > i) {
+            sum1 += +(num[i]);
+        } else {
+            sum2 += +(num[i]);
+        }
+    }
+
+    // console.log(sum1, sum2);
+    return sum1 == sum2;
+}
+// console.log(lackyIs(1071414701));
+
+function normalizeNum (num, digitsAmount) {
+    let strNum = String(num);
+// console.log (strNum);
+    return formStr('0', digitsAmount - strNum.length) + strNum;
 }
 
-// console.log(lackyIs(101200));
+function formStr(data, digitsAmount) {
+    let result = '';
 
-function normalizeNum(num) {
-	let str = String(num);
+    for (let i = 1 ; i <= digitsAmount; i++) {
+        result += data
+    }
 
-	if (str.length == 5) {
-		str = '0' + str;
-	}
-	if (str.length == 4) {
-		str = '00' + str;
-	}
-
-	return str;
+    return result;
 }
 
-// console.log(normalizeNum(21223));
+// console.log(formStr(0, 6));
+
+//console.log(normalizeNum(111, 6));
+
+// console.log(lackyIs(10023, 6));
+
+function startRange (digitsAmount) {
+    strResult ='';
+
+    for(let i = 0; i < digitsAmount / 2 - 1; i++) {
+        strResult += '0';
+    }
+
+    strResult += 1;
+
+    return strResult + strResult;
+}
+
+// console.log(startRange(6));
+
+function endRange (digitsAmount) {
+    strResult = '';
+
+    for (let i = 1; i <= digitsAmount; i++) {
+        strResult += '9';
+    }
+
+    return strResult;
+}
+
+// console.log(endRange(6));
