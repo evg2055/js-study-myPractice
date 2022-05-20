cd77eb12f4fddc628f6f8d780bb6c98856a4356d
EVG2055 <enivanoff55@mail.ru>
2022-05-20 18:58:30 +0600

    done 153 of 312

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-// let arr = ['ret', 'are', 'typ'];
+let arr = [2, 4, 6, 9];
 
-
-// let newArr = arr.map(function(elem) {
-//    return elem.split('').reverse().join('');
+// let check = arr.every ( function (elem, index) {
+//  return elem * index < 30;
 // })
 
-// console.log(newArr);
+let check = arr.every((elem, index) => elem * index < 30);
 
-// let arr = ['123', '456', '789'];
-
-// let newArr = arr.map(function(strNum) {
-//   return strNum.split('');
-// })
-
-// console.log(newArr);
-
-let arr = [1, 2, 3, 4, 5];
-
-let indexArr = arr.map(function(elem, index) {
-  return elem + String(index);
-})
-
-console.log(indexArr);
+console.log(check);
