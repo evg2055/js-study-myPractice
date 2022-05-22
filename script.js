55d18f6194660f958cfa8eb320eb14a5d8b4a86b
EVG2055 <enivanoff55@mail.ru>
2022-05-22 16:11:57 +0600

    done 159 of 312

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// let arr = ['ret', 'are', 'typ'];
+// let arr = [3, 74, 35, 25, 36, 60, 60, 57, 50];
+
+// let max = Math.min(...arr);
+
+// console.log(max);
+
+let result;
+
+function middleMath (...num) {
+    let sum = 0;
+    let amountDigitals = 0;
+
+    for (let elem of num) {
+    sum += elem;
+    amountDigitals++;
+}
+
+    return sum/amountDigitals;
+
+};
+
+console.log(middleMath(3, 74, 35, 25, 36, 60, 60, 57, 50));
 
 
-// let newArr = arr.map(function(elem) {
-//    return elem.split('').reverse().join('');
-// })
-
-// console.log(newArr);
-
-// let arr = ['123', '456', '789'];
-
-// let newArr = arr.map(function(strNum) {
-//   return strNum.split('');
-// })
-
-// console.log(newArr);
-
-let arr = [1, 2, 3, 4, 5];
-
-let indexArr = arr.map(function(elem, index) {
-  return elem + String(index);
-})
-
-console.log(indexArr);
