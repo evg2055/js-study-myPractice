3a4bc2648d6a22fa05250a838d022c52d510fb35
EVG2055 <enivanoff55@mail.ru>
2022-05-24 00:20:44 +0600

    done 174 of 312

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-let now = new Date ();
-let date = new Date (now.getFullYear(), now.getMonth(), 1);
+// let now  = new Date();
+// let year = now.getFullYear();
 
-console.log(date.getDay());
+// for (let month = 0; month <= 11; month++) {
+// 	let date = new Date(year, month, 1);
+	
+// 	if (date.getDay() == 0) {
+// 		console.log(year + '-' + month + '-1');
+// 	}
+// }
+
+
+let now = new Date();
+let count = 0;
+
+for (let year = 2000; year < now.getFullYear() ; year++) {
+    let date = new Date (year, 0);
+
+    if (date.getDay() == 0 || date.getDay() == 6) {
+        console.log(year);
+        count++;
+    }
+}
+
+console.log(count);
