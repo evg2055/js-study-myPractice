05c21ec9584b8899c4248934e17aa9fc079c4f7c
EVG2055 <enivanoff55@mail.ru>
2022-05-24 23:40:07 +0600

    done 177 of 312

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-let brDay = '16.04';
-let conLo = console.log;
+let now = new Date();
+let diner = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12);
 
-if (brDay >= '21.02' && brDay <= '19.03') {
-    console.log('овен');
-}
-
-if (brDay >= '20.03' && brDay <= '20.04') {
-    console.log('телец');
-}
-
-if (brDay >= '21.04' && brDay <= '21.05') {
-    console.log('близнецы');
-}
-
-if (brDay >= '22.05' && brDay <= '22.06') {
-    console.log('рак');
-}
-
-if (brDay >= '23.06' && brDay <= '22.07') {
-    console.log('лев');
-}
-
-if (brDay >= '23.07' && brDay <= '22.08') {
-    conLo('дева');
-}
-
-if (brDay >= '23.08' && brDay <= '23.09') {
-    conLo('весы');
-}
+console.log(now < diner);
