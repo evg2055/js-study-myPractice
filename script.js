e21a18cb2ea924ff833f1061de3b1c865ba4d0d8
EVG2055 <enivanoff55@mail.ru>
2022-05-28 22:58:32 +0600

    done 188 of 312

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const BUTTON = document.querySelector('#button');
-const P = document.querySelector('#p');
+// Пусть у вас есть картинка. Разместите ее на странице в помощью тега img. Пусть даны также кнопка и абзац.
+//  Сделайте так, чтобы по клику на кнопку в абзац записался путь к картинки из ее атрибута src.
 
-BUTTON.addEventListener('dblclick', () => P.innerHTML = '<b>' + P.innerHTML + '</b>');
+const P = document.querySelector('.p');
+const BUTTON = document.querySelector('#button');
+const img = document.querySelector('#img');
+
+BUTTON.addEventListener( 'click', () => console.log(img.innerHTML));
