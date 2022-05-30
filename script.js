dabff069d57bade5136239e163ced6bfd8aba727
EVG2055 <enivanoff55@mail.ru>
2022-05-30 22:10:28 +0600

    done 190 of 312

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Пусть у вас есть картинка. Разместите ее на странице в помощью тега img. Пусть даны также кнопка и абзац.
-//  Сделайте так, чтобы по клику на кнопку в абзац записался путь к картинки из ее атрибута src.
+const ELEM = document.querySelector('#elem');
 
-const P = document.querySelector('.p');
-const BUTTON = document.querySelector('#button');
-const img = document.querySelector('#img');
-
-BUTTON.addEventListener( 'click', () => console.log(img.innerHTML));
+ELEM.addEventListener('blur', () => ELEM.value = '');
