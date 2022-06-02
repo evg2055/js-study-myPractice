8d5091a00c952f9cdf248685362f9930f2a7a83c
EVG2055 <enivanoff55@mail.ru>
2022-06-02 17:33:44 +0600

    193 of 312

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-const BUTTON = document.querySelector('#button');
-const ELEM = document.querySelector('#elem');
-let result = [];
+// let p1 = document.querySelector('.p1');
+// let p2 = document.querySelector('.p2');
+// let p3 = document.querySelector('.p3');
+// let p4 = document.querySelector('.p4');
+// let p5 = document.querySelector('.p5');
 
+// function func() {
+//    this.innerHTML = '!' + this.innerHTML + '!' 
+// }
+ 
+// p1.addEventListener('click', func);
+// p2.addEventListener('click', func);
+// p3.addEventListener('click', func);
+// p4.addEventListener('click', func);
+// p5.addEventListener('click', func);
 
-BUTTON.addEventListener('click', function () {
-    console.log(ELEM.className.split(' '));
-})
+// const INPUT = document.querySelector('#inp');
+
+// function func() {
+//    this.value = this.value*2
+// };
+
+// INPUT.addEventListener('blur', func);
