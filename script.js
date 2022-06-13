3a6cfbce46074964ae3d8a836675db9880cea637
EVG2055 <enivanoff55@mail.ru>
2022-06-13 23:20:35 +0600

    done 207 of 312

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-const DIV = document.querySelector('#div1');
+let P = document.querySelectorAll('p');
 const BUTTON = document.querySelector('#button');
 
-BUTTON.addEventListener('click', () => {
-   DIV.style.width = '100px';
-   DIV.style.height = '100px';
-   DIV.style.backgroundColor = 'yellow';
-   DIV.style.color = 'red';
-   DIV.innerHTML = 'hi';
+BUTTON.addEventListener('click', function func() {
+   let i = 0;
+
+   for(let elem of P) {
+      i++;
+      console.log(elem);
+      elem.setAttribute('data-num', i);
+      elem.innerHTML = elem.innerHTML + elem.getAttribute('data-num');
+}
 })
