0d25f578f88285080dcca359b9fc04195bc62bc9
EVG2055 <enivanoff55@mail.ru>
2022-06-14 23:30:49 +0600

    done 209 of 312

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const ELEM = document.querySelector('#elem');
+const years = document.querySelectorAll('li');
+let years6 = [];
+let sumYears = 0;
 
-for(let nod of ELEM.childNodes) {
-   console.log(nod.data);
-};
+for (let year of years) {
+   const NUM = year.textContent;
+   let sum = 0;
+   for (let i = 0; i < NUM.length; i++){
+      sum += +NUM[i];
+   }
+   sum === 6 ? years6.push(NUM) : null ;
+}
+ for (let year of years6) {
+   sumYears += +year;
+ }
+
+ console.log(sumYears, years6);
