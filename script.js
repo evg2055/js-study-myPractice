79012118f888b8e88d0cf05f06d24ef656845229
EVG2055 <enivanoff55@mail.ru>
2022-06-15 22:04:13 +0600

    done 210 of 312

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-const years = document.querySelectorAll('li');
-let years6 = [];
-let sumYears = 0;
+let inp = document.querySelector('#inp');
 
-for (let year of years) {
-   const NUM = year.textContent;
-   let sum = 0;
-   for (let i = 0; i < NUM.length; i++){
-      sum += +NUM[i];
-   }
-   sum === 6 ? years6.push(NUM) : null ;
-}
- for (let year of years6) {
-   sumYears += +year;
- }
-
- console.log(sumYears, years6);
+inp.addEventListener('blur', function() {
+	let digits = inp.value.split('');
+	let sum = 0;
+	
+	for (let digit of digits) {
+		sum += +digit;
+	}
+	
+	console.log(sum);
+});
