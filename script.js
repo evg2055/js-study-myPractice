a969b4a826e350ddd0515e6cb5b80a013e65ea07
EVG2055 <enivanoff55@mail.ru>
2022-06-18 22:15:53 +0600

    done 211 of 312

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-let inp = document.querySelector('#inp');
+let input = document.querySelector('#first');
+let button = document.querySelector('#button');
+let obj = {1: 'mon', 2: 'tue', 3: 'wed', 4: 'Thu', 5: 'fri', 6: 'sat', 7:' sat'}
 
-inp.addEventListener('blur', function() {
-	let digits = inp.value.split('');
-	let sum = 0;
-	
-	for (let digit of digits) {
-		sum += +digit;
-	}
-	
-	console.log(sum);
-});
+button.addEventListener('click', () => {
+	let date = new Date (input.value);
+	console.log(date, obj[date.getDay()]);
+})
+
