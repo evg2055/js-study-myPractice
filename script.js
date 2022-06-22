1731b0def806f1bbb8c103a734611c477767271d
EVG2055 <enivanoff55@mail.ru>
2022-06-22 16:49:08 +0600

    done 232 of 312

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
-// Привяжите к тегу ul обработчик клика. В этом обработчике проверяйте с помощью свойства tagName,
-//  по какому тегу был клик. Если клик был по тегу li - добавьте в конец текста этого тега восклицательный 
-//  знак. А если клик был по тегу ul - добавьте ему в конец еще один пункт списка.
+const P = document.querySelector('p');
+const inp1 = document.querySelector('#fir');
+const inp2 = document.querySelector('#sec');
+const link = document.querySelector('#elem');
 
-const UL = document.querySelector('#elem');
-let newLi = document.createElement('li');
-
-
-UL.addEventListener('click', (event) => {
-	console.log(event.target.tagName);
-
-	// event.target.tagName === 'LI' ? alert('yes') : alert('null') ;
-
-	if (event.target.tagName === 'LI') {
-		event.target.innerHTML += '!';
-	} else if (event.target.tagName === 'UL'){
-		UL.appendChild(newLi);
-		newLi.textContent = 'text';
-		;
-	}
-} )
+link.addEventListener('click', function func(event) {
+	event.preventDefault();
+	P.textContent = +inp1.value + +inp2.value;
+})
