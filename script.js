3e52601b008dc8ad269f168fbda2ee94c05e8a7e
EVG2055 <enivanoff55@mail.ru>
2022-06-25 17:41:54 +0600

    done 245 of 312

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-let elem = document.querySelector('#elem');
+let elem = document.getElementById('elem');
 
 function func(surname, name) {
 	console.log(this.value + ', ' + surname + ' ' + name);
 }
 
-func.apply(elem, ['Иван', "Иванов"]); // тут должно вывести 'привет, Иванов Иван'
+let newFunc = func.bind(elem);
+
+func.call(elem,'Иванов', 'Иван'); // тут должно вывести 'привет, Иванов Иван'
+newFunc('Петров', 'Петр'); // тут должно вывести 'привет, Петров Петр'
