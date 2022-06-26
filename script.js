4b036842875f8028846676833281be244ab4f4e5
EVG2055 <enivanoff55@mail.ru>
2022-06-26 21:46:49 +0600

    done 250 of 312

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-'use strict'
+'use strict';
 
-const BUTTON = document.querySelector('#button');
+const START = document.querySelector('#start');
+const STOP = document.querySelector('#stop');
+const DISP = document.querySelector('#display');
+let timerId;
+let setTimer = 10;
 
-BUTTON.addEventListener('click', function func() {
-	 let i = 10;
+function func() {
+	timerId = setInterval(() => {
+		START.removeEventListener('click', func)
+		if(setTimer > 0 ) {
+			DISP.value = setTimer--;
+		} else {
+			clearInterval(timerId);
+			DISP.value = 'время истекло!';
+		}
+	}, 1000)
+}
 
-	 setInterval(() => console.log(--i), 1000);
+START.addEventListener('click', func)
 
-	 this.removeEventListener('click', func);
-});
-
+STOP.addEventListener('click', () => {
+	START.addEventListener('click', func);
+	clearInterval(timerId);
+	DISP.value = setTimer  + ' pause';
+})
