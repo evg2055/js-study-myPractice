19f1a69131d8cf4cf1e5e2f29df88f7b44f1d9b9
EVG2055 <enivanoff55@mail.ru>
2022-07-06 23:23:30 +0600

    done 266 of 312

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 'use strict';
 
-const Paragraph = document.querySelector('#paragraph');
-
-let i = 1;
-let j = 1000;
-
-function func() {
-	setTimeout(() => {
-		console.log(i++);
-		j += 1000;
-		func();
-
-	}, j)
-}
-
-func();
