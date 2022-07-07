f96492655914dae8e07947263769a6b8385979d3
EVG2055 <enivanoff55@mail.ru>
2022-07-07 15:06:55 +0600

    done 270 of 312

--- script.js
+++ script.js
@@ -4,15 +4,22 @@ let arr = [[1,4,5,5],[3,545,32,223,544],[344,2223,442]];
 const TABLE = document.querySelector('#table');
 let BUTTON =document.querySelector('#button');
 
+
 for(let subarr of arr) {
     let newTr = document.createElement('tr');
     for(let elem of subarr) {
         let newTd = document.createElement('td');
         newTd.innerHTML = elem;
-        BUTTON.addEventListener('click', () => {
-            newTd.innerHTML *= 2;
-        });
         newTr.appendChild(newTd);
     }
     TABLE.appendChild(newTr);
 }
+let tdArr = document.querySelectorAll('#table td');
+
+for(let i = 0; i <= tdArr.length-1; i++) {
+    tdArr[i].addEventListener('click', (event) => {
+        event.target.innerHTML = tdArr.length - i
+    })
+}
+
+
