e59238e524ef75eadbd492ae4788c34d5c5eddf6
EVG2055 <enivanoff55@mail.ru>
2022-07-08 15:17:19 +0600

    done 272 of 312

--- script.js
+++ script.js
@@ -1,43 +1,6 @@
-'use strict';
+const ELEM = document.querySelector('#elem');
 
-let arr = [[1,4,5,5],[3,545,32,223,544],[344,2223,442]];
-const TABLE = document.querySelector('#table');
-let BUTTON =document.querySelector('#button');
-
-
-for(let subarr of arr) {
-    let newTr = document.createElement('tr');
-    for(let elem of subarr) {
-        let newTd = document.createElement('td');
-        newTd.innerHTML = elem;
-        newTr.appendChild(newTd);
-    }
-    TABLE.appendChild(newTr);
-}
-
-let tdArr = document.querySelectorAll('#table td');
-let i = 1;
-
-for(let elem of tdArr) {
-    elem.addEventListener('click', () => {
-    
-    let eL = elem.innerHTML;
-    
-    
-
-    if (eL == '2' || eL == '3') {
-        eL = i;
-    } 
-    if (eL == '1' || eL == '3') {
-        i = 2;
-        eL = i;
-    }
-    if (eL == '1' || eL == '2') {
-        i = 3;
-        eL = i;
-    } else {elem.innerHTML = i;
-    alert(eL)}
-    elem.innerHTML = i;
-    })
-
-}
+ELEM.addEventListener('click', () => {
+    alert('!')
+    ELEM.remove();
+})
