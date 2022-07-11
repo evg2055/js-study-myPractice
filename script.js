96ef85a623922754f305318e0dff8c0151f1b5ef
EVG2055 <enivanoff55@mail.ru>
2022-07-12 00:26:58 +0600

    done 276 of 312

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const ELEM = document.querySelector('#elem');
-const REMOVE = document.querySelector('#remove');
+const TABLE = document.querySelector('table');
+const TRS = document.querySelectorAll('table tr');
+const TDS = document.querySelectorAll('table td');
 
-REMOVE.addEventListener('click', function (event) {
-    ELEM.parentElement.removeChild(ELEM);
-    event.preventDefault();
+for(let elem of TRS) {
+    let addTD = document.createElement('td');
+    let remove = document.createElement('a');
+    remove.href = '';
+    remove.innerHTML = 'remove';
+    remove.addEventListener('click', (event) => {
+        addTD.parentElement.remove(event.target.parentElement);
+        // console.log(elem.parentElement, event.target.parentElement)
+        event.preventDefault();
+    })
+    addTD.appendChild(remove);
+    elem.appendChild(addTD);
+}
 
-})
