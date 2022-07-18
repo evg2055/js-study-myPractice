e1e00c6465ee8f7b49857b6698922db7ef50b093
EVG2055 <enivanoff55@mail.ru>
2022-07-18 22:22:21 +0600

    done 6 of 60

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-class User {};
-let USER1 = new User;
-let USER2 = new User;
-let USER3 = new User;
+class SALARY {
+    sum(digital) {
+        return digital > 2;
+    }
+}
 
-USER1.name = 'eric';
-USER2.name = 'john';
-USER3.name = 'crack';
-USER1.salary = 100;
-USER2.salary = 200;
-USER3.salary = 300;
+let user = new SALARY;
 
-console.log(USER1.salary + USER2.salary + USER3.salary)
+console.log(user.sum(1))
