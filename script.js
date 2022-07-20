d0a0a89e750e46fd689c7941fa0c925098ace2b8
EVG2055 <enivanoff55@mail.ru>
2022-07-20 22:01:46 +0600

    done 10 of 60

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-class stud {
-    name;
-    surn;
-
-    show() {
-       return this.name + '  ' + this.surn;
+class employ{
+    constructor() {
+        console.log("+++");
     }
 }
-
-let stud1 = new stud;
-stud1.name = 'bill';
-stud1.surn = 'geit'
-
-console.log(stud1.show())
+new employ;
